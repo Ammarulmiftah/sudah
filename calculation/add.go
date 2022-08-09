72db93a2ed84dd790b0053d411f30995de66f9cc
Ammarulmiftah <ammarmiftah23@gmail.com>
2022-08-09 14:33:18 +0800

    ini sudah bisa

---
+++ calculation/add.go
@@ -0,0 +1,5 @@
+package calculation
+
+func Add(name string, nameTwo string, nameThree string) string {
+	return name + nameTwo + nameThree
+}
---
+++ tugas_yt.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"pertama/calculation"
+)
+
+func main() {
+	result := calculation.Add("ini", "", "saya")
+	fmt.Println(result)
+
+}
